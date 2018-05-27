2ff7b292b3a5de589c708ebde744c3ffad648b9d
Mayoor Chopasni School <virajmathur2002@zoho.com>
2018-05-27 15:29:29 +0530

    experiment again

--- assets/js/main.js
+++ assets/js/main.js
@@ -303,4 +303,8 @@
 
 	});
 
+	$(document).ready(function(){
+		// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+		$('.modal').modal();
+	  });
 })(jQuery);
