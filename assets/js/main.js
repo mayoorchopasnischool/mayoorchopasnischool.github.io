8f3f3108fdc0a46ccfab5bacb3ba262cff7c6977
Mayoor Chopasani School <virajmathur2002@zoho.com>
2018-05-28 01:16:43 +0530

    [13] WIP cca_clubs

--- assets/js/main.js
+++ assets/js/main.js
@@ -306,5 +306,7 @@
 	$(document).ready(function(){
 		// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 		$('.modal').modal();
+		$('#modal1').modal('open');
 	  });
+
 })(jQuery);
